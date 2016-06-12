960a0ba0e47a0ca289b065f67d6a16954a3ba878
Rushi Sangani <rushisangani@gmail.com>
2016-06-12 23:42:22 +0530

    Initial commit RSMasterKit
    files added

---
+++ RSMasterKit/RSMasterKit/RSMasterKitConstants.h
@@ -0,0 +1,96 @@
+//
+// RSMasterKitConstants.h
+// Copyright (c) 2016 Rushi Sangani
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <Foundation/Foundation.h>
+
+
+/********************** RSNetworkKit ************************/
+
+#import "RSNetworkKitConstants.h"
+
+
+/********************** RSMasterTableView ************************/
+
+#import "RSMasterTableView.h"
+
+
+/********************** RSWebSearchTableView ************************/
+
+#import "RSWebSearchTableView.h"
+
+
+/********************** RSAlertViewController ************************/
+
+#import "RSAlertViewController.h"
+
+
+/********************** RSWordDetectorTextView ************************/
+
+#import "RSWordDetectorTextView.h"
+
+
+/********************** Components ************************/
+
+#import "RSUserDefaults.h"
+#import "RSImageTextField.h"
+
+
+/********************** RSSelectionViewController ************************/
+
+#import "RSSelectionViewController.h"
+
+
+/********************** RSSegmentViewController ************************/
+
+#import "RSSegmentViewController.h"
+
+
+/********************** RSDatePickerController ************************/
+
+#import "RSDatePickerController.h"
+
+
+/********************** Categories ************************/
+
+
+#import "NSString+Additions.h"
+
+#import "NSDictionary+RemoveNull.h"
+
+#import "NSDate+Formatter.h"
+
+#import "NSObject+AssociatedObject.h"
+#import "NSObject+Properties.h"
+
+#import "UIColor+Additions.h"
+
+#import "UITextField+Additions.h"
+
+#import "UIView+Additions.h"
+
+#import "UIViewController+AlertController.h"
+
+
+
+@interface RSMasterKitConstants : NSObject
+
+@end
